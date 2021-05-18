ea369a3bf8cc25fd15b727b3bd0f99c35693d2b8
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-05-18 17:51:48 -0300

    typo fix in imports on categories repository

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,9 +1,10 @@
 import { Category } from "../../models/Category";
-import { ICategoriesRepository, ICreateCategoryDTO } from "../ICategoriesRepository";
-
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../ICategoriesRepository";
 
 class CategoriesRepository implements ICategoriesRepository {
-
   private categories: Category[];
 
   private static INSTANCE: CategoriesRepository;
@@ -25,7 +26,7 @@ class CategoriesRepository implements ICategoriesRepository {
     Object.assign(category, {
       name,
       description,
-      created_at: new Date()
+      created_at: new Date(),
     });
 
     this.categories.push(category);
@@ -36,7 +37,7 @@ class CategoriesRepository implements ICategoriesRepository {
   }
 
   findByName(name: string): Category {
-    const category = this.categories.find(category => category.name === name);
+    const category = this.categories.find((category) => category.name === name);
     return category;
   }
 }
