2ca5da1990dbfdb129ad06ab490e71b159c21039
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-05-18 18:50:10 -0300

    database configuration

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
+import cors from "cors";
 import express from "express";
-import { router } from "./routes";
 import swaggerUi from "swagger-ui-express";
+
+import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
+import "./database";
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use(router)
+app.use(router);
 
-app.listen(3333, () => console.log("Server is running on port http://localhost:3333"));
+app.listen(3333, () =>
+  console.log("Server is running on port http://localhost:3333")
+);
