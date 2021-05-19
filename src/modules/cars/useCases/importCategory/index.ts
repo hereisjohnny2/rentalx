7a39cf6ecf72513bbff52eed9b5aaeebdb7d8db5
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-05-19 12:10:39 -0300

    refactor create category use case to index file

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -2,8 +2,10 @@ import { CategoriesRepository } from "../../repositories/implementations/Categor
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
-const categoriesRepository = CategoriesRepository.getInstance();
+const categoriesRepository = null;
 const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
-const importCategoryController = new ImportCategoryController(importCategoryUseCase);
+const importCategoryController = new ImportCategoryController(
+  importCategoryUseCase
+);
 
-export { importCategoryController }
+export { importCategoryController };
