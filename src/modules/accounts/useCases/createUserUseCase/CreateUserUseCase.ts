0dad277e08037a76e2fd0c77f3e1fb13a840a5d1
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-05-20 10:07:40 -0300

    Create User useCase

---
+++ src/modules/accounts/useCases/createUserUseCase/CreateUserUseCase.ts
@@ -0,0 +1,30 @@
+import { inject, injectable } from "tsyringe";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../respositories/IUsersRepository";
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private userRepository: IUsersRepository
+  ) {}
+
+  async execute({
+    name,
+    username,
+    email,
+    password,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    await this.userRepository.create({
+      name,
+      username,
+      email,
+      password,
+      driver_license,
+    });
+  }
+}
+
+export { CreateUserUseCase };
---
+++ src/routes/users.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+import { CreateUserController } from "../modules/accounts/useCases/createUserUseCase/CreateUser.Controller";
+
+const usersRoutes = Router();
+
+const createUserController = new CreateUserController();
+
+usersRoutes.post("/", createUserController.handle);
+
+export { usersRoutes };
