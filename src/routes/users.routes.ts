9ed08510356e5fae516da1f255662d08e42af347
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-05-21 10:42:44 -0300

    upload routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,14 +1,22 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "../config/upload";
 import { CreateUserController } from "../modules/accounts/useCases/createUserUseCase/CreateUser.Controller";
 import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const usersRoutes = Router();
 
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUserController.handle);
-usersRoutes.patch("/avatar", updateUserAvatarController.handle);
+usersRoutes.patch(
+  "/avatar",
+  uploadAvatar.single("avatar"),
+  updateUserAvatarController.handle
+);
 
 export { usersRoutes };
