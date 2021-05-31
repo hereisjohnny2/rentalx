9673b380a966606460383b0cb61b30edccd291a2
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-05-31 15:12:35 -0300

    entities refactoring to go inside the infra layer

--- src/modules/accounts/respositories/implementations/UsersRepository.ts
+++ src/modules/accounts/respositories/implementations/UsersRepository.ts
@@ -1,7 +1,7 @@
 import { getRepository, Repository } from "typeorm";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
-import { User } from "../../entities/User";
+import { User } from "../../infra/typeorm/entities/User";
 import { IUsersRepository } from "../IUsersRepository";
 
 class UsersRepository implements IUsersRepository {
