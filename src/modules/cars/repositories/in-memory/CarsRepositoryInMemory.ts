14a0b209dffafabc653a59c94865a3f530931573
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-06-01 09:21:22 -0300

    create car use case tested for a in memory repository

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -2,7 +2,7 @@ import { ICreateCarDTO } from "../../dtos/ICreateCarDTO";
 import { Car } from "../../infra/typeorm/entities/Car";
 import { ICarsRepository } from "../ICarsRepository";
 
-class ClassRepositoryIMemory implements ICarsRepository {
+class CarsRepositoryInMemory implements ICarsRepository {
   private cars: Car[] = [];
   async create({
     name,
@@ -12,7 +12,7 @@ class ClassRepositoryIMemory implements ICarsRepository {
     fine_amount,
     brand,
     category_id,
-  }: ICreateCarDTO): Promise<void> {
+  }: ICreateCarDTO): Promise<Car> {
     const car = new Car();
 
     Object.assign(car, {
@@ -26,7 +26,13 @@ class ClassRepositoryIMemory implements ICarsRepository {
     });
 
     this.cars.push(car);
+
+    return car;
+  }
+
+  async findByLicencePlate(licence_plate: string): Promise<Car> {
+    return this.cars.find((car) => car.licence_plate === licence_plate);
   }
 }
 
-export { ClassRepositoryIMemory };
+export { CarsRepositoryInMemory };
---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,64 @@
+import { AppError } from "../../../../shared/errors/AppError";
+import { CarsRepositoryInMemory } from "../../repositories/in-memory/CarsRepositoryInMemory";
+import { CreateCarUseCase } from "./CreateCarUseCase";
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepository: CarsRepositoryInMemory;
+
+describe("Create Car", () => {
+  beforeEach(() => {
+    carsRepository = new CarsRepositoryInMemory();
+    createCarUseCase = new CreateCarUseCase(carsRepository);
+  });
+
+  it("should be able to create a new car", async () => {
+    const car = await createCarUseCase.execute({
+      name: "Car Name",
+      description: "Car Description",
+      daily_rate: 100,
+      licence_plate: "ABC-1234",
+      fine_amount: 60,
+      brand: "Car Brand",
+      category_id: "category",
+    });
+
+    expect(car).toHaveProperty("id");
+  });
+
+  it("should not be able to create a car with a existent licence plate", () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: "Car 1",
+        description: "Car Description",
+        daily_rate: 100,
+        licence_plate: "ABC-1234",
+        fine_amount: 60,
+        brand: "Car Brand",
+        category_id: "category",
+      });
+
+      await createCarUseCase.execute({
+        name: "Car 2",
+        description: "Car Description",
+        daily_rate: 100,
+        licence_plate: "ABC-1234",
+        fine_amount: 60,
+        brand: "Car Brand",
+        category_id: "category",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should create a car with availability by default", async () => {
+    const car = await createCarUseCase.execute({
+      name: "Car 1",
+      description: "Car Description",
+      daily_rate: 100,
+      licence_plate: "ABC-1234",
+      fine_amount: 60,
+      brand: "Car Brand",
+      category_id: "category",
+    });
+    expect(car.available).toBe(true);
+  });
+});
