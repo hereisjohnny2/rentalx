a3fd4afc41a179b2e376150c23d7a80a0bdb044c
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-06-01 14:25:52 -0300

    seed to create admin user

---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,21 @@
+import { hash } from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
+
+import createConnection from "..";
+
+async function create() {
+  const connection = await createConnection("localhost");
+
+  const id = uuidv4();
+  const password = await hash("admin", 8);
+
+  await connection.query(
+    `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+    values('${id}', 'admin', 'admin@rentalx.com', '${password}', true, 'now()', 'ABCD')
+    `
+  );
+
+  await connection.close();
+}
+
+create().then(() => console.log("User Admin Created"));
