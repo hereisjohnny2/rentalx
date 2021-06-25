32656dc83ca93b65cad6a44d9b50c5803b867e43
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-06-25 13:46:01 -0300

    method to get the images urls

--- src/modules/accounts/infra/typeorm/entities/User.ts
+++ src/modules/accounts/infra/typeorm/entities/User.ts
@@ -1,3 +1,4 @@
+import { Expose } from "class-transformer";
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
@@ -30,6 +31,18 @@ class User {
   constructor() {
     if (!this.id) {
       this.id = uuidV4();
+    }
+  }
+
+  @Expose({ name: "avatar_url" })
+  avatar_url(): string {
+    switch (process.env.disk) {
+      case "local":
+        return `${process.env.APP_API_URL}/avatar/${this.avatar}`;
+      case "s3":
+        return `${process.env.AWS_BUCKET_URL}/avatar/${this.avatar}`;
+      default:
+        return null;
     }
   }
 }
--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -6,6 +6,7 @@ import "reflect-metadata";
 import "dotenv/config";
 
 import "../../container";
+import upload from "../../../config/upload";
 import swaggerFile from "../../../swagger.json";
 import { AppError } from "../../errors/AppError";
 import createConnection from "../typeorm";
@@ -17,6 +18,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 app.use(router);
 
 app.use(
