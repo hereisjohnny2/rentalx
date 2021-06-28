4a964f626eba9042c489b1c652816e45a8f09f8c
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-06-28 11:52:30 -0300

    sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,11 +1,16 @@
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
-import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
+
+import "express-async-errors";
+
 import "reflect-metadata";
 import "dotenv/config";
 
 import "../../container";
+
 import upload from "../../../config/upload";
 import swaggerFile from "../../../swagger.json";
 import { AppError } from "../../errors/AppError";
@@ -16,13 +21,39 @@ import { router } from "./routes";
 createConnection();
 
 const app = express();
+
 app.use(cors());
+
+Sentry.init({
+  dsn: process.env.SENTRY_DNS,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(rateLimiter);
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 app.use(router);
+
+app.use(
+  Sentry.Handlers.errorHandler({
+    shouldHandleError(error) {
+      if (error.status === 429 || error.status === 500) {
+        return true;
+      }
+      return false;
+    },
+  })
+);
 
 app.use(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
